30f71629c293712ddfdcb7e556cd62d42842fead
Brain Atoms <badams6453@gmail.com>
2023-06-29 20:57:28 -0400

    updated file structure, added typeDefs, resolvers, qu eries and mutations

--- caesar/server/models/Entertainment.js
+++ caesar/server/models/Entertainment.js
@@ -1,4 +1,4 @@
-const { Schema } = require('mongoose');
+const { Schema } = require("mongoose");
 
 // This is a subdocument schema, it won't become its own model but we'll use it as the schema for the User's `savedBooks` array in User.js
 const entertainmentSchema = new Schema({
@@ -19,4 +19,4 @@ const entertainmentSchema = new Schema({
   },
 });
 
-module.exports = bookSchema;
+module.exports = entertainmentSchema;
