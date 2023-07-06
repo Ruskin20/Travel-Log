0fa840cec50002dd13c1a879b3f78a467bd5b94e
Brain Atoms <badams6453@gmail.com>
2023-07-06 19:11:58 -0400

    updated models, typeDefs and resolvers

--- caesar/server/schemas/typeDefs.js
+++ caesar/server/schemas/typeDefs.js
@@ -5,6 +5,78 @@ const typeDefs = gql`
     _id: ID!
     username: String!
     email: String!
-    bookCount: Int
-    savedBooks: [Book]
+    savedRestaurants: [Restaurant],
+    savedEntertainment: [Entertainment]
   }
+
+  type Restaurant {
+    restaurantId: String!
+    restaurant_name: String!
+    description: String!
+    image: String
+    link: String
+  }
+
+  type Entertainment {
+    entertainmentId: String!
+    venue_name: String!
+    description: String!
+    image: String
+    link: String
+  }
+
+  input RestaurantInput {
+    restaurantId: String!
+    restaurant_name: String!
+    description: String!
+  }
+
+  input EntertainmentInput {
+    entertainmentId: String!
+    venue_name: String!
+    description: String!
+  }
+
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    loginUser(
+      email: String!, 
+      password: String!
+    ): Auth
+
+    addUser(
+      username: String!, 
+      email: String!, 
+      password: String!
+    ): Auth
+
+    saveRestaurant(
+      restaurantId: String!,
+      description: String!,
+      restaurant_name: String!,
+      image: String,
+      link: String
+    ): User
+
+    saveEntertainment(
+      entertainmentId: String!,
+      description: String!,
+      venue_name: String!,
+      image: String,
+      link: String
+    ): User
+    
+    removeRestaurant(
+      restaurantId: String!
+    ): User
+
+    removeEntertainment(
+      entertainmentId: String!
+    ): User
+  }
+`;
+
+module.exports = typeDefs;
