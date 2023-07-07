849e3ffef4e7444c7adb17e5b4b75b8948c62829
Blake Sanders <blakesand00@gmail.com>
2023-07-06 21:25:53 -0400

    error handling and refresh

--- caesar/server/utils/auth.js
+++ caesar/server/utils/auth.js
@@ -1,18 +1,17 @@
 const jwt = require("jsonwebtoken");
+const { AuthenticationError } = require('apollo-server-express');
 
 const secret = "mysecretsshhhhh";
 const expiration = "2h";
 
 module.exports = {
-  authMiddleware: function ({ req }) {
-    // Allows token to be sent via req.body, req.query, or headers
+  authMiddleware: function ({ req, res }) {
     let token = req.body.token || req.query.token || req.headers.authorization;
 
-    // ["Bearer", "<tokenvalue>"]
+    // Extract the token value from the "Bearer" scheme
     if (req.headers.authorization) {
       token = token.split(" ").pop().trim();
     }
-
     if (!token) {
       return req;
     }
@@ -20,8 +19,20 @@ module.exports = {
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
-    } catch {
+      // Check if token is about to expire 
+      const currentTime = Math.floor(Date.now() / 1000);
+      const { exp } = jwt.decode(token);
+      const timeUntilExpiration = exp - currentTime;
+
+      // If token is about to expire, generate a new token and send it as a response header
+      if (timeUntilExpiration < 300) {
+        const newToken = this.signToken(data);
+        res.set('Authorization', newToken);
+      }
+
+    } catch (err) {
       console.log("Invalid token");
+      throw new AuthenticationError("Invalid token");
     }
 
     return req;
