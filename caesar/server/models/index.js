370c3cd9bd0f84e21f591de81636e25a1057a8ec
Brain Atoms <badams6453@gmail.com>
2023-07-07 18:54:27 -0400

    made sure saveRestaurant and saveVenue functions work

--- caesar/server/models/index.js
+++ caesar/server/models/index.js
@@ -7,5 +7,4 @@ module.exports = {
     User,
     Entertainment,
     Restaurant,
-
 };
--- caesar/server/utils/auth.js
+++ caesar/server/utils/auth.js
@@ -5,13 +5,18 @@ const expiration = "2h";
 
 module.exports = {
   authMiddleware: function ({ req }) {
+    console.log(req.body)
     // Allows token to be sent via req.body, req.query, or headers
     let token = req.body.token || req.query.token || req.headers.authorization;
+    console.log("token1")
+    console.log(token)
 
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
       token = token.split(" ").pop().trim();
     }
+    console.log('token2')
+    console.log(token)
 
     if (!token) {
       return req;
@@ -19,6 +24,8 @@ module.exports = {
 
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
+      console.log('data')
+      console.log(data)
       req.user = data;
     } catch {
       console.log("Invalid token");
