307834c7396e3ca2f88e70451b7276c41ca5203c
Matthew Standish <mwstandish@aol.com>
2023-07-11 19:10:50 -0400

    mapbox template

--- caesar/client/src/App.js
+++ caesar/client/src/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import Navbar from './componets/Navbar';
-import './App.css';
-import Home from './componets/pages/Home';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Option1 from './componets/pages/Option1';
-import Option2 from './componets/pages/Option2';
-import Signup from './componets/pages/SignUp';
+import React from "react";
+import Navbar from "./componets/Navbar";
+import "./App.css";
+import Home from "./componets/pages/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Food from "./componets/pages/Food";
+import Option2 from "./componets/pages/Option2";
+import Signup from "./componets/pages/SignUp";
 
 function App() {
   return (
     <>
       <Router>
         <Navbar />
-       
+
         <Switch>
-          <Route path='/' exact component={Home} />
-          <Route path='/option1' component={Option1} />
-          <Route path='/option2' component={Option2} />
-          <Route path='/sign-up' component={Signup} />
-          
+          <Route path="/" exact component={Home} />
+          <Route path="/option1" component={Food} />
+          <Route path="/option2" component={Option2} />
+          <Route path="/sign-up" component={Signup} />
         </Switch>
       </Router>
     </>
--- caesar/client/src/componets/pages/Option1.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import '../../App.css';
-
-export default function Option1() {
-  return <h1 className='option1'>Option1</h1>;
-}
