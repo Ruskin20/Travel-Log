4f632212031194891b85bfda87af08de4bc2b696
Brain Atoms <badams6453@gmail.com>
2023-07-12 12:44:11 -0400

    got signup and login to successfully work with graphql and added a logout button

--- caesar/client/src/App.js
+++ caesar/client/src/App.js
@@ -6,11 +6,39 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Food from "./componets/pages/Food";
 import Activities from "./componets/pages/Activities";
 import Adventure from "./componets/pages/Adventure";
-import Signup from "./componets/pages/SignUp";
+import Signup, {Login} from "./componets/pages/SignUp";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
     <>
+      <ApolloProvider client={client}>
       <Router>
         <Navbar />
 
@@ -20,8 +48,10 @@ function App() {
           <Route path="/option2" component={Activities} />
           <Route path="/option3" component={Adventure} />
           <Route path="/sign-up" component={Signup} />
+          <Route path="/login" component={Login} />
         </Switch>
-      </Router>
+        </Router>
+        </ApolloProvider>
     </>
   );
 }
--- caesar/client/src/componets/pages/SignUp.js
+++ caesar/client/src/componets/pages/SignUp.js
@@ -1,10 +1,144 @@
-import React from 'react';
-import '../../App.css';
-import SignUpPage from '../SignUp';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useMutation } from '@apollo/client';
+import Auth from '../../utils/auth';
+import { ADD_USER } from '../../utils/mutations';
+import { LOGIN_USER } from '../../utils/mutations';
+import '../SignUp.css';
 
+export function Login(props) {
+  const [formState, setFormState] = useState({ email: '', password: ''});
+  const [loginUser, { error }] = useMutation(LOGIN_USER);
 
-export default function SignUp() {
-  return(<>
-  <SignUpPage />
-  </>)
+  const handleFormSubmit = async (event) => {
+    console.log('howdy')
+    event.preventDefault();
+    try {
+      const mutationResponse = await loginUser({
+        variables: { email: formState.email, password: formState.password },
+      });
+      const token = mutationResponse.data.loginUser.token;
+      Auth.login(token);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  return (
+    <div id="Login" className="container my-1">
+
+      <h2>Login</h2>
+      <form onSubmit={handleFormSubmit}>
+        <div className="flex-row space-between my-2">
+          <label htmlFor="email">Email address:</label>
+          <input
+            placeholder="youremail@test.com"
+            name="email"
+            type="email"
+            id="email"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="flex-row space-between my-2">
+          <label htmlFor="pwd">Password:</label>
+          <input
+            placeholder="******"
+            name="password"
+            type="password"
+            id="pwd"
+            onChange={handleChange}
+          />
+        </div>
+        {error ? (
+          <div>
+            <p className="error-text">The provided credentials are incorrect</p>
+          </div>
+        ) : null}
+        <div className="flex-row flex-end">
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+    </div>
+  );
 }
+
+function Signup(props) {
+  const [formState, setFormState] = useState({ email: '', password: '', username: '' });
+  const [addUser] = useMutation(ADD_USER);
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    const mutationResponse = await addUser({
+      variables: {
+        email: formState.email,
+        password: formState.password,
+        username: formState.username,
+      },
+    });
+    const token = mutationResponse.data.addUser.token;
+    Auth.login(token);
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  return (
+    <div id="SignUp" className="container my-1">
+
+      <h2>Signup</h2>
+      <form onSubmit={handleFormSubmit}>
+        <div className="flex-row space-between my-2">
+          <label htmlFor="userName">Username:</label>
+          <input
+            placeholder="Username"
+            name="username"
+            type="Username"
+            id="username"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="flex-row space-between my-2">
+          <label htmlFor="email">Email:</label>
+          <input
+            placeholder="youremail@test.com"
+            name="email"
+            type="email"
+            id="email"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="flex-row space-between my-2">
+          <label htmlFor="pwd">Password:</label>
+          <input
+            placeholder="******"
+            name="password"
+            type="password"
+            id="pwd"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="flex-row flex-end">
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+
+
+export default Signup;
+
