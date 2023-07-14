707ce959a2f7d611eda23bc6d98606e04e9b52a4
Matthew Standish <mwstandish@aol.com>
2023-07-14 13:35:52 -0400

    route error

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,11 +1,15 @@
-const router = require('express').Router();
-const path = require('path');
-const apiRoutes = require('./api');
+const router = require("express").Router();
+const path = require("path");
+const apiRoutes = require("./api");
 
-router.use('/api', apiRoutes);
+router.use("/api", apiRoutes);
 
-router.use((req, res) => {
-  res.sendFile(path.join(__dirname, '../../client/build/index.html'));
+// Serve the client-side application for the root URL
+router.use("/", express.static(path.join(__dirname, "../../client/build")));
+
+// Fallback route for all other URLs
+router.use("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../client/build/index.html"));
 });
 
 module.exports = router;
