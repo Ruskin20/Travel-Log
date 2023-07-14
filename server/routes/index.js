15a1b15526ab3b0440cea948f32f49c55adc61d7
Matthew Standish <mwstandish@aol.com>
2023-07-14 13:51:23 -0400

    routes error

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,10 +4,8 @@ const apiRoutes = require("./api");
 
 router.use("/api", apiRoutes);
 
-// Serve the client-side application for the root URL
 router.use("/", express.static(path.join(__dirname, "../../client/build")));
 
-// Fallback route for all other URLs
 router.use("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../../client/build/index.html"));
 });
